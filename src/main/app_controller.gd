b99a7e75105f5dbb5b0a6207983f01c7353cfee2
Andy Dalaimo <adalaimo@gmail.com>
2026-04-29 14:13:05 +0100

    Adding Splash screen

--- src/main/app_controller.gd
+++ src/main/app_controller.gd
@@ -35,6 +35,8 @@ func _ready():
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("EscapeArcade"):
 		get_tree().quit()
+	if event.is_action_pressed("RestartArcade"):
+		get_tree().reload_current_scene()
 
 ## Sets root path on initializing App
 func init_app_data_path() -> void:
